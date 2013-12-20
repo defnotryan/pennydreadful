3fd11133a185543d419712e41558749d7bd0d5ef
defnotryan <ryan@theredremedy.com>
2013-12-19 21:58:57 -0500

    Authz test for project mutation

--- src/pennydreadful/auth.clj
+++ src/pennydreadful/auth.clj
@@ -1,6 +1,11 @@
 (ns pennydreadful.auth
   (:require [cemerick.friend :as friend]
-            [pennydreadful.data.user :as data-user]))
+            [pennydreadful.util :refer [not-nil?]]
+            [pennydreadful.data.user :as data-user]
+            [pennydreadful.data.project :as data-project]))
 
 (defn credentials [username]
   (data-user/user-for-username username))
+
+(defn user-eid-can-mutate-project-eid? [user-eid project-eid]
+  (not-nil? (data-project/project-eid-owned-by-user-eid? user-eid project-eid)))
